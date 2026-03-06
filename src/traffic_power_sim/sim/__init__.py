84e4a5fd6c830fab9759087d7511441c4c07f6ad
Chao Yan <chaorandyan@gmail.com>
2026-03-06 16:29:52 +0800

    Implement core event loop for traffic power simulation

---
+++ src/traffic_power_sim/sim/__init__.py
@@ -0,0 +1,23 @@
+from .core_event_loop import (
+    Action,
+    CoreEventLoop,
+    Event,
+    EventType,
+    LogEntry,
+    MetricSnapshot,
+    SimState,
+    SimulationResult,
+    TaskStatus,
+)
+
+__all__ = [
+    "Action",
+    "CoreEventLoop",
+    "Event",
+    "EventType",
+    "LogEntry",
+    "MetricSnapshot",
+    "SimState",
+    "SimulationResult",
+    "TaskStatus",
+]
